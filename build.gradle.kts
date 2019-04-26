8f3cdf467064c2c09acca4ab1590c593a1416a7f
mikand13 <mikkelsen.anders@gmail.com>
2019-04-26 12:28:50 +0200

    resolve issue with lang proxies not being compiled

--- build.gradle.kts
+++ build.gradle.kts
@@ -191,7 +191,6 @@ subprojects {
     kapt {
         correctErrorTypes = true
         useBuildCache = true
-        includeCompileClasspath = false
 
         javacOptions {
             option("-Xdoclint:none")
